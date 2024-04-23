3a9663826ca93bf7bdd09a1252d5f0ff1548ef6f
KhaznaKhalifatunesya <guineverrearts@gmail.com>
2024-04-23 11:28:03 +0700

    sdd

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -4,7 +4,11 @@
 @endpush
 
 @section('content')
-
+    <div class="container my-5">
+        <div class="shadow p-3 mb-5 bg-body-tertiary rounded-4">
+            @livewire('home')
+        </div>
+    </div>
 @endsection
 
 @push('js')
